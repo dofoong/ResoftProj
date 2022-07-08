6552de8822c458d8e53e4291bdd3d7873a8c9114
Min Joo Cho <mjoo1320@gmail.com>
2022-07-08 19:02:09 +0900

    3-2

---
+++ js/project.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+$('.slider').slick('setPosition');
+
+$('.prd').slick({
+    infinite: true,
+    slidesToShow: 7,
+    slidesToScroll: 1
+  });
